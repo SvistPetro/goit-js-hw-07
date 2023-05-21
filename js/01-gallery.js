10e701a8f6adb780a5c4905a378fd34127c6da81
SvistPetro <svist.petro@gmail.com>
2023-05-21 20:15:42 +0300

    Завдання 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+const galleryElements = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></li>`
+);
+ulEl.insertAdjacentHTML("beforeend", galleryElements.join(""));
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const imgUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${imgUrl}" width="800" height="600">
+    `);
+  instance.show();
+}
+
+ulEl.addEventListener("click", onClick);
