03847d4ccea300821af5e87636b2b2849e6e6573
SvistPetro <svist.petro@gmail.com>
2023-05-21 21:50:14 +0300

    Завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+const galleryElements = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a></li>`
+);
+ulEl.insertAdjacentHTML("beforeend", galleryElements.join(""));
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionPosition: "bottom",
+    captionDelay: 250,
+    captionsData: "alt",
+  });
+}
+
+ulEl.addEventListener("click", onClick);
